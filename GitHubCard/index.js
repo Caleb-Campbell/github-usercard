40d16a9a047ef17618965699de4016f6ec7c160e
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-08-30 10:10:19 -0600

    Completed all tasks

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,24 @@
+import axios from 'axios'
+
+const followersArray = ['Caleb-Campbell', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+function makeCards(username){
+  axios.get(`https://api.github.com/users/${username}`)
+  .then(resp => {
+    document.querySelector('.cards').appendChild(cardMaker(resp.data));
+    
+  })
+  .catch(err => {console.error(err)})
+
+}
+
+for (let i = 0; i < followersArray.length; i++){
+  makeCards(followersArray[i])
+}
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -17,6 +33,10 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
+
+
+
+
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,11 +48,71 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
+
+*/
+
+function cardMaker(dataObj){
+
+  const cardWrapper = document.createElement('div')
+  cardWrapper.classList.add('card')
+
+  // child img
+  const userImg = document.createElement('img')
+  userImg.src = dataObj.avatar_url
+
+  // another child of cardwrapper  
+  const cardInfo = document.createElement('div')
+  cardInfo.classList.add('card-info')
+
+  const heading = document.createElement('h3')
+  heading.classList.add('name')
+  heading.textContent = dataObj.name
+
+  const username = document.createElement('p')
+  username.classList.add('username')
+  username.textContent = dataObj.login
+
+  const location = document.createElement('p')
+  location.textContent = dataObj.location
+
+  const profileLink = document.createElement('p')
+
+  //nested child link in profileLink
+  const pageLink = document.createElement('a')
+  pageLink.href = dataObj.html_url
+
+  const followers = document.createElement('p')
+  followers.textContent = `Followers: ${dataObj.followers}`
+
+  const following = document.createElement('p')
+  following.textContent = `Following: ${dataObj.following}`
+
+  const bio = document.createElement('p')
+  bio.textContent = dataObj.bio
+  bio.style.fontWeight = 'bold'
+
+  cardWrapper.appendChild(userImg)
+  cardWrapper.appendChild(cardInfo)
+  cardInfo.appendChild(heading)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profileLink)
+  profileLink.appendChild(pageLink)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  return cardWrapper
+
+}
+/*
+
+
 
     <div class="card">
       <img src={image url of user} />
